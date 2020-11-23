e7a7a2543677ba195e568c3e18205b993931fba1
Steven Sussman <sjsussman@gmail.com>
2020-11-23 14:02:50 -0500

    installed dependencies

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom'
+import PrivateRoute from '../src/components/PrivateRoute.js'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h1>Test Render</h1>
+    </div>
+  );
+}
+
+export default App;
