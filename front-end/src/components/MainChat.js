6298f2590e61114f701f5b6ce7e2ca7a6d6dd045
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-11-26 09:23:03 -0600

    Chatroom Styling moved to Css file

--- front-end/src/components/MainChat.js
+++ front-end/src/components/MainChat.js
@@ -2,14 +2,19 @@ import React, { useContext } from "react";
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { AuthContext } from "./Auth"
 import fireApp from '../base.js'
+
+import '../App.css'
+
+
+
+
 const firestore = fireApp.firestore();
 
 
 const MainChat = () => {
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt').limit(55);
   const [messages] = useCollectionData(query, { idField: 'id' });
-  // console.log("these are the messages", messages)
 
   return (<>
     <main>
@@ -21,13 +26,14 @@ const MainChat = () => {
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
   const messageClass = uid === useContext(AuthContext).uid ? 'sent' : 'received';
-  console.log('chat message current user', useContext(AuthContext))
 
   return (<>
-    <div className={`message ${messageClass}`}>
-      <img style={{ width: '3%', borderRadius:15,}} src={photoURL || 'https://media-exp1.licdn.com/dms/image/C4D03AQE4tHxILDIRTA/profile-displayphoto-shrink_200_200/0?e=1610582400&v=beta&t=UqBSpNh_OnwIzDTX04QJTkoPFqqI7phAGy0SuzvsJSA'} alt="stock" />
-      <span style={{}}>{text}</span>
-    </div>
+    {/* <div className={`message ${messageClass}`}> */}
+      <div className="icon" >
+      <span className='text' ><img src={photoURL || 'https://media-exp1.licdn.com/dms/image/C4D03AQE4tHxILDIRTA/profile-displayphoto-shrink_200_200/0?e=1610582400&v=beta&t=UqBSpNh_OnwIzDTX04QJTkoPFqqI7phAGy0SuzvsJSA'} alt="stock" />
+      {text}</span></div>
+      
+      {/* </div> */}
   </>)
 }
 
