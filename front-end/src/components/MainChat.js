8d072ab414c8d5c5a73d94003164c838e5109d14
Steven Sussman <sjsussman@gmail.com>
2020-11-28 17:08:45 -0500

    fixed photoURL

--- front-end/src/components/MainChat.js
+++ front-end/src/components/MainChat.js
@@ -37,18 +37,9 @@ const MainChat = () => {
 
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
-  let photoHolder
   const messageClass = uid === useContext(AuthContext).currentUser.uid? 'sent' : 'received';
-  // if(messageClass==='received')
-  // {
-  //   photoHolder='https://avatars1.githubusercontent.com/u/51976194?s=460&u=44b1c04deb6995b58523c9192945fe1ca646fe6c&v=4'
-  // }
-  // else{
-  //   photoHolder='https://media-exp1.licdn.com/dms/image/C4D03AQE4tHxILDIRTA/profile-displayphoto-shrink_200_200/0?e=1610582400&v=beta&t=UqBSpNh_OnwIzDTX04QJTkoPFqqI7phAGy0SuzvsJSA'
-  //  }
   
   return (<>
-
       <div className={`${messageClass}`}>
       <div className="icon" >
       <img src={photoURL || "https://image.shutterstock.com/image-vector/profile-placeholder-image-gray-silhouette-260nw-1637863831.jpg" } alt="stock" />
